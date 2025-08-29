73dccbd0f6efd8259387b360016a86ddb24baa6a
nicktwocheers <nick.twocheers@gmail.com>
2025-08-29 12:53:02 +0900

    utagUpdate250829

--- my-next-site/lib/markdownToHtml.js
+++ my-next-site/lib/markdownToHtml.js
@@ -4,12 +4,21 @@ import { remark } from 'remark'
 import html from 'remark-html'
 import gfm from 'remark-gfm'
 import slug from 'remark-slug'  // 追加
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
+import rehypeStringify from 'rehype-stringify'
 
 export default async function markdownToHtml(markdown) {
   const result = await remark()
     .use(gfm)          // GitHub flavored markdown（テーブルなど）
     .use(slug)         // 見出しにIDをつける
     .use(html)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)        // Markdown内のHTMLタグをそのままHTML変換
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .process(markdown)
   return result.toString()
 }
+
